a1f98b0f73f83a4bbfd37cfc9402f9a87da82aab
Thomas Khyn <6889068+tkhyn@users.noreply.github.com>
2014-04-04 12:59:45 +1300

    Ooops, tests must have their own app

--- runtests.py
+++ runtests.py
@@ -3,10 +3,10 @@ import sys
 from tests import settings as SETTINGS
 from django.conf import settings
 
-settings.configure(**{k:getattr(SETTINGS,k) for k in dir(SETTINGS) \
+settings.configure(**{k:getattr(SETTINGS, k) for k in dir(SETTINGS) \
                           if k[0].isupper()})
 
 from django.test.simple import DjangoTestSuiteRunner
 
 test_runner = DjangoTestSuiteRunner(verbosity=1)
-test_runner.run_tests(['idmapper', ])
+test_runner.run_tests(['tests', ])
--- tests/settings.py
+++ tests/settings.py
@@ -1,16 +1,17 @@
 DEBUG = True
 
-DATABASES= {
+DATABASES = {
     'default': {
         'NAME': 'idmapper',
         'ENGINE': 'django.db.backends.sqlite3',
     }
 }
 
-INSTALLED_APPS=(
+INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.admin',
     'idmapper',
+    'tests',
 )
