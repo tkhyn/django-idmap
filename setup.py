6b6e7e173b10e1fc7f23d8ecf4c8bfb239649059
Thomas Khyn <6889068+tkhyn@users.noreply.github.com>
2014-09-28 17:26:20 +1300

    distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
+"""
+django-idmap
+An identity mapper for the Django ORM
+(c) 2014 Thomas Khyn
+(c) 2009 David Cramer
+Simplified BSD License (see LICENSE.txt)
+"""
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 import os
-
-INC_PACKAGES = 'idmap',  # string or tuple of strings
-EXC_PACKAGES = ()  # tuple of strings
-
-install_requires = (
-  'django>=1.5',
-)
 
 
 # imports __version__ and __version_info__ without importing module
@@ -21,19 +20,24 @@ if dev_status == 'alpha' and not __version_info__[4]:
 DEV_STATUS = {'pre': '2 - Pre-Alpha',
               'alpha': '3 - Alpha',
               'beta': '4 - Beta',
-              'rc': '5 - Production/Stable',
+              'rc': '4 - Beta',
               'final': '5 - Production/Stable'}
 
-metadata = dict(
+setup(
     name='django-idmap',
     version=__version__,
-    author='David Cramer',
-    author_email='dcramer@gmail.com',
-    url='http://github.com/dcramer/django-idmap',
-    description='An identify mapper for the Django ORM',
+    author='Thomas Khyn',
+    author_email='thomas@ksytek.com',
+    url='https://bitbucket.org/tkhyn/django-idmap',
+    description='An identity mapper for the Django ORM',
+    long_description=open(os.path.join('README.rst')).read(),
+    keywords=['django', 'identity', 'mapper'],
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Development Status :: %s' % DEV_STATUS[dev_status],
         'License :: OSI Approved :: BSD License',
         'Framework :: Django',
@@ -42,36 +46,8 @@ metadata = dict(
         'Topic :: Software Development',
         'Topic :: Database'
     ],
+    packages=find_packages(exclude=('tests',)),
+    install_requires=(
+      'django>=1.4',
+    ),
 )
-
-# packages parsing from root packages, without importing sub-packages
-root_path = os.path.dirname(__file__)
-if isinstance(INC_PACKAGES, basestring):
-    INC_PACKAGES = (INC_PACKAGES,)
-
-packages = []
-excludes = list(EXC_PACKAGES)
-for pkg in INC_PACKAGES:
-    pkg_root = os.path.join(root_path, *pkg.split('.'))
-    for dirpath, dirs, files in os.walk(pkg_root):
-        rel_path = os.path.relpath(dirpath, pkg_root)
-        pkg_name = pkg
-        if (rel_path != '.'):
-            pkg_name += '.' + rel_path.replace(os.sep, '.')
-        for x in excludes:
-            if x in pkg_name:
-                continue
-        if '__init__.py' in files:
-            packages.append(pkg_name)
-        elif dirs:  # stops package parsing if no __init__.py file
-            excludes.append(pkg_name)
-
-
-def read(filename):
-    return open(os.path.join(root_path, filename)).read()
-
-setup(**dict(metadata,
-   packages=packages,
-   long_description=read('README.rst'),
-   install_requires=install_requires,
-))
