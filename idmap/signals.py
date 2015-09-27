669fec4a8dbd16409098e3f0d3c8fdb15ba32445
Thomas Khyn <6889068+tkhyn@users.noreply.github.com>
2015-09-27 15:12:03 +1300

    Adds pre- and post- flush signals

--- idmap/signals.py
+++ idmap/signals.py
@@ -3,9 +3,12 @@ Signals are used to automatically flush the idmap cache on finish request,
 migrate / syncdb and instance deletion, and to make sure to catch cascades
 """
 
-from django.dispatch import receiver
+from django.dispatch import Signal, receiver
 from django.core.signals import request_finished
 from django.db.models.signals import pre_delete, post_migrate
+
+pre_flush_idmap = Signal()
+post_flush_idmap = Signal()
 
 
 @receiver((post_migrate, request_finished))
@@ -14,8 +17,10 @@ def flush_cache(**kwargs):
     Flushes the idmap cache on migrate and on request end
     """
     from .models import SharedMemoryModel
+    pre_flush_idmap.send(None)
     for model in SharedMemoryModel.__subclasses__():
         model.flush_instance_cache(flush_sub=True)
+    post_flush_idmap.send(None)
 
 
 @receiver(pre_delete)
