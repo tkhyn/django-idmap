e46460d76729d188fe7caeb77ae4dc91b8b7af6a
Thomas Khyn <6889068+tkhyn@users.noreply.github.com>
2015-09-30 22:34:40 +1300

    Fixes SharedMemoryManager

--- idmap/manager.py
+++ idmap/manager.py
@@ -1,11 +1,8 @@
-from django.db.models.manager import Manager
+from django.db.models.manager import BaseManager
 
 from .queryset import SharedMemoryQuerySet
 
 
-class SharedMemoryManager(Manager):
+class SharedMemoryManager(BaseManager.from_queryset(SharedMemoryQuerySet)):
 
     use_for_related_fields = True
-
-    def get_queryset(self):
-        return SharedMemoryQuerySet(self.model, using=self._db)
